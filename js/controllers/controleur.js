83fd48e30c02a6874dc5da0df1b962c3cde754aa
Malo Boeda <malo.boeda@gmail.cm>
2020-04-28 15:57:41 +0200

    Ca y est ca marche

--- js/controllers/controleur.js
+++ js/controllers/controleur.js
@@ -2,6 +2,7 @@ var rennesData = angular.module("rennesData", []);
 
 rennesData.controller("controleur", ['$scope',
     function($scope) {
+      $scope.dataPrint = [];
 
       var requestURL = "https://data.rennesmetropole.fr/api/records/1.0/search/?dataset=resultats_c11";
 
@@ -12,22 +13,15 @@ rennesData.controller("controleur", ['$scope',
 
       request.onload = function() {
         var data = request.response;
-        $scope.dataPrint = loadData(JSON.parse(data));
+        loadData(JSON.parse(data));
 
-        function loadData(data){
-          dataTab = [];
-          for (i = 0; i < data.records.length; i++){
-            dataTab.push({ nom_circonscription : data.records[i].fields.nom_circonscription });
-          }
-          console.log(dataTab);
-          return dataTab;
-        }
-
-        console.log($scope.dataPrint);
       };
-      
-      console.log($scope.dataPrint);
 
-
+      function loadData(data){
+        for (i = 0; i < data.records.length; i++){
+          $scope.dataPrint.push(data.records[i].fields);
+        }
+      }
     }
+
 ]);
